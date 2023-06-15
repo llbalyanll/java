4c8eca508bf310defd970e25936ef4ac0c3c04d0
llbalyanll <balyanarsen6@gmail.com>
2023-06-15 18:36:19 +0400

    arsen

---
+++ src/das6/test6.java
@@ -0,0 +1,38 @@
+package das6;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class test6 {
+    public static void main(String[] args) {
+        //  for (int i = 100;i >20;i --)
+        //   System.out.println(i );
+
+        //  boolean isTrue =true;
+        //  int j = 0;
+        // while (j<100){
+        // j +=1;
+        // if (j%2 ==0)
+        //  continue;
+        // System.out.println(j );
+
+        //break;return;continue;
+        //  int x =0;
+        // do {
+        // x ++;
+        // System.out.println(x);
+        // } while (x < 0);
+//      for (int i = 0; i < 100; i+=10) {
+//         System.out.println(i);
+//      } for (int i = 0; i < name.length; i++)
+
+
+       // float[] name=new float[]{10.54f,20.35f,30.87f,40.54f};
+       // System.out.println(name[1]);
+
+        }
+    }
+
+
+
+
