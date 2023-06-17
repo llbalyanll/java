7f875342aba1981f63690b631c337d0216ec0b2b
llbalyanll <balyanarsen6@gmail.com>
2023-06-18 00:11:13 +0400

    arsen

--- src/das4/Main4.java
+++
@@ -1,7 +0,0 @@
-package das4;
-
-public class Main4 {
-    public static void main(String[] args) {
-
-    }
-}
---
+++ src/das9/OOP.java
@@ -0,0 +1,13 @@
+package das9;
+
+import das4.Person;
+
+public class OOP {
+    public static void main(String[] args) {
+        Person arsen= new Person();
+         arsen.say("Ero");
+        System.out.println(arsen.height);
+
+     }
+}
+
