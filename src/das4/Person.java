3a869a30f29e9bea701bd18fdfbad61eb79ed177
llbalyanll <balyanarsen6@gmail.com>
2023-06-18 22:55:37 +0400

    arsen

--- src/das4/Person.java
+++ src/das4/Person.java
@@ -3,9 +3,15 @@ package das4;
 public class Person {
 
         public int height = 180;
+        public String name = "Default";
         public void say (String name){
             System.out.println("Hello,"+name);
         }
+        public Person (int h,String n){
+            name= n;
+            height = h;
+        }
+
      }
 
 
--- src/das9/OOP.java
+++ src/das9/OOP.java
@@ -4,7 +4,7 @@ import das4.Person;
 
 public class OOP {
     public static void main(String[] args) {
-        Person arsen= new Person();
+        Person arsen= new Person(120,"Barev");
          arsen.say("Ero");
         System.out.println(arsen.height);
 
